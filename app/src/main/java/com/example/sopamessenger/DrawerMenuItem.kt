4026126c8ba975c7e34df649c658e8226f494e5e
YuraSopa <aspiresopa@gmail.com>
2024-10-10 16:54:27 +0300

    added NavigationDrawer.kt

---
+++ app/src/main/java/com/example/sopamessenger/DrawerMenuItem.kt
@@ -0,0 +1,10 @@
+package com.example.sopamessenger
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class DrawerMenuItem(
+    val id: String,
+    val title: String,
+    val contentDescription: String,
+    val icon: ImageVector
+)
