1cc313f15ed21d1b6f34ca6045017d526a238d27
YuraSopa <aspiresopa@gmail.com>
2024-10-10 16:54:27 +0300

    added NavigationDrawer.kt

--- app/src/main/java/com/example/sopamessenger/navigation/ScreenRoutes.kt
+++ app/src/main/java/com/example/sopamessenger/navigation/ScreenRoutes.kt
@@ -3,5 +3,4 @@ package com.example.sopamessenger.navigation
 sealed class ScreenRoutes(val route: String) {
     object SignInScreen : ScreenRoutes(route = "SignIn_Screen")
     object SignUpScreen : ScreenRoutes(route = "SignUp_Screen")
-    object MainScreen : ScreenRoutes(route = "Main_Screen")
 }
