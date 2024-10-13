a5d10c152876e06bc60c14281b7f96f20e1d5118
YuraSopa <aspiresopa@gmail.com>
2024-10-13 14:47:13 +0300

    Added chat, remove 1 activity, change navigation.

--- app/src/main/java/com/example/sopamessenger/navigation/NavigationGraph.kt
+++ app/src/main/java/com/example/sopamessenger/navigation/NavigationGraph.kt
@@ -2,9 +2,13 @@ package com.example.sopamessenger.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.sopamessenger.presentation.chat_screen.ChatScreen
+import com.example.sopamessenger.presentation.chat_screen.HomeScreen
 import com.example.sopamessenger.presentation.login_screen.SignInScreen
 import com.example.sopamessenger.presentation.signup_screen.SignUpScreen
 
@@ -21,6 +25,18 @@ fun NavigationGraph(
         }
         composable(route = ScreenRoutes.SignUpScreen.route) {
             SignUpScreen(navController)
+        }
+        composable(route = ScreenRoutes.HomeScreen.route){
+            HomeScreen(navController)
+        }
+        
+        composable(route = "chat/{channelId}", arguments = listOf(
+            navArgument("channelId") {
+                type = NavType.StringType
+            }
+        )) {
+            val channelId = it.arguments?.getString("channelId") ?: ""
+            ChatScreen(navController, channelId)
         }
     }
 }
